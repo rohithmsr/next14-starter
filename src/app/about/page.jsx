082a0a70da43df71394689254101dcb02c4a1826
rohithmsr <rohit.rajaram@arcadia.com>
2024-09-14 20:36:20 +0530

    adding images with protocol settings

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className={styles.imgContainer}>
+      <Image src="/about.png" alt="About Image" fill className={styles.img} />
+    </div>
+  );
 };
 
 export default AboutPage;
