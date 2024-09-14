4527effe087212744b3697f4518bc29aab969dfb
rohithmsr <rohit.rajaram@arcadia.com>
2024-09-14 13:36:45 +0530

    navbar setup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/blog">Blogs</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
