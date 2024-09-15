3787350044689b7e0d5540e375798b7eb68b0a7d
rohithmsr <rohit.rajaram@arcadia.com>
2024-09-15 10:08:20 +0530

    render single post

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,84 @@
-const Page = () => {
-  return <div>Single Post Page</div>;
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+// import PostUser from "@/components/postUser/postUser";
+// import { Suspense } from "react";
+// import { getPost } from "@/lib/data";
+
+// FETCH DATA WITH AN API
+// const getData = async (slug) => {
+//   const res = await fetch(`http://localhost:3000/api/blog/${slug}`);
+
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
+
+//   return res.json();
+// };
+
+// export const generateMetadata = async ({ params }) => {
+//   const { slug } = params;
+
+//   const post = await getPost(slug);
+
+//   return {
+//     title: post.title,
+//     description: post.desc,
+//   };
+// };
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+
+  // FETCH DATA WITH AN API
+  // const post = await getData(slug);
+  const post = {
+    img: "https://images.pexels.com/photos/762041/pexels-photo-762041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "Post title",
+    desc: "My dear thala",
+    body: "Lorem ipsum, ilthakasa iya,thakal",
+    createdAt: "2024-04-21",
+  };
+
+  // FETCH DATA WITHOUT AN API
+  // const post = await getPost(slug);
+
+  return (
+    <div className={styles.container}>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          {post && (
+            // <Suspense fallback={<div>Loading...</div>}>
+            //   <PostUser userId={post.userId} />
+            // </Suspense>
+            <Image
+              src={post.img}
+              alt=""
+              width={50}
+              height={50}
+              className={styles.img}
+            />
+          )}
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Tobey Maguire</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(4, 16)}
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.desc}</div>
+      </div>
+    </div>
+  );
 };
 
-export default Page;
+export default SinglePostPage;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,41 @@
+import PostCard from "../../components/postCard/PostCard";
+import styles from "./blog.module.css";
+
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className={styles.container}>
+      <PostCard
+        post={{
+          img: "https://images.pexels.com/photos/762041/pexels-photo-762041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          title: "Post title",
+          desc: "My dear thala",
+          body: "Lorem ipsum, ilthakasa iya,thakal",
+          createdAt: "2024-04-21",
+          slug: "post-slug-1",
+        }}
+      />
+      <PostCard
+        post={{
+          img: "https://images.pexels.com/photos/762041/pexels-photo-762041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          title: "Post title",
+          desc: "My dear thala",
+          body: "Lorem ipsum, ilthakasa iya,thakal",
+          createdAt: "2024-04-21",
+          slug: "post-slug-2",
+        }}
+      />
+      <PostCard
+        post={{
+          img: "https://images.pexels.com/photos/762041/pexels-photo-762041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          title: "Post title",
+          desc: "My dear thala",
+          body: "Lorem ipsum, ilthakasa iya,thakal",
+          createdAt: "2024-04-21",
+          slug: "post-slug-3",
+        }}
+      />
+    </div>
+  );
 };
 
 export default BlogPage;
