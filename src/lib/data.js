37324e42ed0e5d0cf44a2e4cf3cc94bfcb2a08e3
rohithmsr <rohit.rajaram@arcadia.com>
2024-09-29 13:34:41 +0530

    no cache in methods without fetch

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,6 @@
 import { Post, User } from "./models";
 import { connectToDb } from "./utils";
+import { unstable_noStore as noStore } from "next/cache";
 
 // TEMPORARY DATA
 // const users = [
@@ -37,6 +38,7 @@ export const getPost = async (slug) => {
 };
 
 export const getUser = async (id) => {
+  noStore();
   try {
     connectToDb();
     const user = await User.findById(id);
