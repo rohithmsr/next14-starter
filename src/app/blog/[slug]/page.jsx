d71945d7cfa49002ad474af9ba8a80f7d77e0fc8
rohithmsr <rohit.rajaram@arcadia.com>
2024-09-29 11:05:52 +0530

    adding seprate req fr user info in sep component

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
-// import PostUser from "@/components/postUser/postUser";
-// import { Suspense } from "react";
+import PostUser from "@/components/postUser/postUser";
+import { Suspense } from "react";
 // import { getPost } from "@/lib/data";
 
 // FETCH DATA WITH AN API
-// const getData = async (slug) => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
 
-//   if (!res.ok) {
-//     throw new Error("Something went wrong");
-//   }
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
 
-//   return res.json();
-// };
+  return res.json();
+};
 
 // export const generateMetadata = async ({ params }) => {
 //   const { slug } = params;
@@ -30,45 +30,29 @@ const SinglePostPage = async ({ params }) => {
   const { slug } = params;
 
   // FETCH DATA WITH AN API
-  // const post = await getData(slug);
-
-  const post = {
-    img: "https://images.pexels.com/photos/762041/pexels-photo-762041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    title: "Post title",
-    desc: slug + " x My dear thala !",
-    body: "Lorem ipsum, ilthakasa iya,thakal",
-    createdAt: "2024-04-21",
-  };
+  const post = await getData(slug);
 
   // FETCH DATA WITHOUT AN API
   // const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
-      {post.img && (
-        <div className={styles.imgContainer}>
-          <Image src={post.img} alt="" fill className={styles.img} />
-        </div>
-      )}
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/762041/pexels-photo-762041.jpeg"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           {post && (
-            // <Suspense fallback={<div>Loading...</div>}>
-            //   <PostUser userId={post.userId} />
-            // </Suspense>
-            <Image
-              src={post.img}
-              alt=""
-              width={50}
-              height={50}
-              className={styles.img}
-            />
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
           )}
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author</span>
-            <span className={styles.detailValue}>Tobey Maguire</span>
-          </div>
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>
@@ -76,7 +60,7 @@ const SinglePostPage = async ({ params }) => {
             </span>
           </div>
         </div>
-        <div className={styles.content}>{post.desc}</div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
