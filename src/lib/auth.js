bdfc01537f92856e3638473011a86fec8cf90764
rohithmsr <rohit.rajaram@arcadia.com>
2024-10-12 15:38:20 +0530

    oauth callback new user to our db

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { connectToDb } from "./utils";
+import { User } from "./models";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -10,6 +12,30 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === "github") {
+        try {
+          connectToDb();
+          const user = await User.findOne({ email: profile.email });
+
+          if (!user) {
+            const newUser = new User({
+              email: profile.email,
+              username: profile.login,
+              img: profile.avatar_url,
+            });
+
+            await newUser.save();
+          }
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 };
 
 export const {
