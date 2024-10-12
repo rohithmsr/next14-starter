a3bb2e4f1e7d82e51fcb76aa4f4a4cec99c2ae44
rohithmsr <rohit.rajaram@arcadia.com>
2024-10-12 13:40:13 +0530

    next auth login with github setup

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,5 +1,22 @@
-const Page = () => {
-  return <div>LoginPage</div>;
+import { auth, signIn } from "@/lib/auth";
+
+const Page = async () => {
+  const handleGithubLogin = async () => {
+    "use server";
+
+    await signIn("github");
+  };
+
+  const session = await auth();
+  console.log("session: ", session);
+
+  return (
+    <div>
+      <form action={handleGithubLogin}>
+        <button type="submit">Login with Github</button>
+      </form>
+    </div>
+  );
 };
 
 export default Page;
